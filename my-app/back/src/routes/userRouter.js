29088cf9b5df300c332f4d5312d444f8cf70276b
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-05-07 10:50:37 +0900

    commit

--- my-app/back/src/routes/userRouter.js
+++ my-app/back/src/routes/userRouter.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const userRouter = Router();
 const User = require("../models/User");
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
 
@@ -36,6 +36,17 @@ userRouter.post("/login", async function (req, res) {
     console.log("/user/login back 진입");
     //바디에 email, password 실어서 요청했다.
     const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).send({ message: "이메일 확인해주세요" });
+    }
+    const isMatch = await compare(req.body.password, user.password);
+    console.log(req.body.password);
+    console.log(user.password);
+    console.log(isMatch);
+    if (!isMatch) {
+      return res.status(400).send({ message: "비밀번호 확인하세요" });
+    }
+
     //user 찾고 토큰 만든다.
 
     const payload = {
@@ -50,9 +61,9 @@ userRouter.post("/login", async function (req, res) {
 
     return res
       .status(200)
-      .send({ user, accessToken, message: "로그인 성공!!!" });
+      .send({ user, accessToken, message: "!!!로그인 성공!!!" });
   } catch (error) {
-    return res.status(400).send({ error: "로그인 실패" });
+    return res.status(400).send({ message: "!!!로그인 실패!!!" });
   }
 });
 
@@ -67,7 +78,15 @@ userRouter.post("/auth", auth, async function (req, res) {
       name: req.user.name,
       role: req.user.role,
     };
-    return res.status(200).send({ user });
+    return res.status(200).send({ user, message: "로그인 확인" });
+  } catch (error) {
+    return res.status(400).send({ error: "로그인 유저 아님" });
+  }
+});
+
+userRouter.post("/logout", auth, async function (req, res) {
+  try {
+    return res.status(200).send({ message: "로그아웃 되셨습니다" });
   } catch (error) {
     return res.status(400).send({ error: error.message });
   }
